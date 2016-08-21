10d76e6d343957f0969e5bc574a2dd96f8d10146
Andy Rimmer <andy@chalumeau-2.local>
2016-08-21 16:29:18 +0100

    Added switch function to parse sub-set of VCF fields

--- Go/vcfParser.go
+++ Go/vcfParser.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "io"
     "os"
-    "bufio"
+    "strconv"
+    "strings"
 )
 
 
-type vcf_record struct {
+type VCFRecord struct {
     contig string
     position int64
+    id int64
     ref string
     alt string
     quality float64
@@ -18,6 +21,30 @@ type vcf_record struct {
     info map[string]string
     format string
     sample_data map[string]string
+}
+
+
+func create_vcf_record_from_string(input_string string) *VCFRecord {
+    record := new(VCFRecord)
+
+    for index,field := range strings.Split(input_string, "\t") {
+        switch index {
+            case 0:
+                record.contig = field
+            case 1:
+                record.position, _ = strconv.ParseInt(field, 0, 64)
+            case 2:
+                record.id, _ = strconv.ParseInt(field, 0, 64)
+            case 3:
+                record.ref = field
+            case 4:
+                record.alt = field
+            case 5:
+                record.quality, _ = strconv.ParseFloat(field, 64)
+        }
+    }
+
+    return record
 }
 
 
@@ -31,6 +58,8 @@ func main() {
             line, _, error_code := input_file_reader.ReadLine()
             var vcf_line string = string(line); // Convert to text from binary
             fmt.Println(vcf_line);
+            vcf_record := create_vcf_record_from_string(vcf_line)
+            fmt.Printf("%+v\n", vcf_record)
 
             if error_code == io.EOF {
                 break;
